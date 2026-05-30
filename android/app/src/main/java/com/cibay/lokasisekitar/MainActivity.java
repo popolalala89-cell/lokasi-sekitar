f415a9cc9caa6a373752306f432d2834a9ddda9b
Pa Popo <cibay89@users.noreply.github.com>
2026-05-30 09:26:52 +0700

    Fix: Add Android resources (manifest, MainActivity, strings, styles, icons)

---
+++ android/app/src/main/java/com/cibay/lokasisekitar/MainActivity.java
@@ -0,0 +1,8 @@
+package com.cibay.lokasisekitar;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    // Capacitor app extends BridgeActivity
+    // All web-to-native communication handled by Capacitor
+}
